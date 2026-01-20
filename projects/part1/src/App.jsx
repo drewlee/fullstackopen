5cc35916720568a77da00b0199d78dd8052000b9
Andrew A Lee <andrew@andrewleedev.com>
2026-01-19 16:30:12 -0800

    Adds examples from part 1c

--- projects/part1/src/App.jsx
+++ projects/part1/src/App.jsx
@@ -1,13 +1,41 @@
-function Hello(props) {
-  console.log(props);
+import { useState } from 'react';
+
+const Hello = ({ name, age }) => {
+  const bornYear = () => new Date().getFullYear() - age
+
   return (
     <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
+      <p>Hello {name}, you are {age} years old</p>
+      <p>So you were probably born in {bornYear()}</p>
     </div>
   );
 }
 
-function App() {
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ onClick, text }) => {
+  return <button onClick={onClick}>{text}</button>
+}
+
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
+  console.log('rendering with counter value', counter)
+
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+
+  const decreaseByOne = () => {
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
+
   const name = 'Peter'
   const age = 10
 
@@ -16,6 +44,20 @@ function App() {
       <h1>Greetings</h1>
       <Hello name='Maya' age={26 + 10} />
       <Hello name={name} age={age} />
+
+      <Display counter={counter} />
+      <Button
+        onClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        onClick={setToZero}
+        text='zero'
+      />
+      <Button
+        onClick={decreaseByOne}
+        text='minus'
+      />
     </div>
   )
 }
