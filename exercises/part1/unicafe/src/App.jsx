09958659b45d8a4bebd8ed42c6ef54f0a2d8cf1a
Andrew A Lee <andrew@andrewleedev.com>
2026-01-24 01:06:13 -0800

    Completes exercise 1.11

--- exercises/part1/unicafe/src/App.jsx
+++ exercises/part1/unicafe/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-const Button = ({ text, onClick }) => <button onClick={onClick}>{text}</button>
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
 const StatisticsLine = ({ label, value }) => {
   return (
@@ -12,10 +12,10 @@ const StatisticsLine = ({ label, value }) => {
 }
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
   const total = good + neutral + bad
   const average = ((good + bad * -1) / total).toFixed(2);
   const positive = `${(good / total * 100).toFixed(2)}%`;
