784abe2d78c2936df3627c80785656370602c8b5
Andrew A Lee <andrew@andrewleedev.com>
2026-01-24 00:01:18 -0800

    Completes exercise 1.7

--- exercises/part1/unicafe/src/App.jsx
+++ exercises/part1/unicafe/src/App.jsx
@@ -9,6 +9,9 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const total = good + neutral + bad
+  const average = ((good + bad * -1) / total).toFixed(2);
+  const positive = (good / total * 100).toFixed(2);
 
   return (
     <>
@@ -25,6 +28,9 @@ const App = () => {
         <p>Good {good}</p>
         <p>Neutral {neutral}</p>
         <p>Bad {bad}</p>
+        <p>All {total}</p>
+        <p>Average {average}</p>
+        <p>Positive {positive}%</p>
       </section>
     </>
   )
