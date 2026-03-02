e03175f7b5cb74e6e7da9120b6f83b9fa3b640e7
Andrew A Lee <andrew@andrewleedev.com>
2026-03-02 13:50:54 -0800

    Completes exercise 2.9

--- exercises/part2/phonebook/src/App.jsx
+++ exercises/part2/phonebook/src/App.jsx
@@ -2,33 +2,45 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '39-44-5323523'
-    }
-  ]) 
+    { name: 'Arto Hellas', number: '39-44-5323523', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
-  const handleFormSubmit = (event) => {
+  const handleFormSubmit = event => {
     event.preventDefault()
 
-    if (newName) {
-      const hasNewName = persons.some(person => person.name === newName)
+    const normNewName = newName.trim()
+    const normNewNumber = newNumber.trim()
 
-      if (hasNewName) {
-        alert(`${newName} is already added to phonebook`)
-      } else {
-        const newEntry = {
-          name: newName,
-          number: newNumber
-        }
-
-        setPersons(persons.concat(newEntry))
-        setNewName('')
-        setNewNumber('')
-      }
+    if (!normNewName || !normNewNumber) {
+      alert('Must include name and number')
+      return
     }
+
+    const hasNewName = persons.some(person => person.name === normNewName)
+    if (hasNewName) {
+      alert(`${normNewName} is already added to phonebook`)
+      return
+    }
+
+    const newEntry = {
+      name: normNewName,
+      number: normNewNumber,
+      id: persons.length + 1,
+    }
+
+    setPersons(persons.concat(newEntry))
+    setNewName('')
+    setNewNumber('')
+  }
+
+  const handleFilterChange = event => {
+    setFilter(event.target.value)
   }
 
   const handleNameChange = event => {
@@ -39,10 +51,26 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const getFilteredNames = () => {
+    const normFilter = filter.trim().toLowerCase()
+    return persons.filter(person => {
+      return person.name.toLocaleLowerCase().includes(normFilter)
+    })
+  }
+
+  const shownPersons = filter
+    ? getFilteredNames()
+    : persons
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with:
+        <input type="text" onChange={handleFilterChange} value={filter} />
+      </div>
       <form onSubmit={handleFormSubmit}>
+        <h2>Add a new</h2>
         <div>
           name: <input type="text" onChange={handleNameChange} value={newName} />
         </div>
@@ -55,11 +83,10 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(
-          person => <li key={person.name}>{person.name}: {person.number}</li>
+        {shownPersons.map(
+          person => <li key={person.id}>{person.name}: {person.number}</li>
         )}
       </ul>
-      <div>debug: {newName} {newNumber}</div>
     </div>
   )
 }
