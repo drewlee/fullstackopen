45c08577a1fcf4a2d1737f3d82a7a897c772fa9b
Andrew A Lee <andrew@andrewleedev.com>
2026-03-04 14:51:19 -0800

    Completes lab 2d

--- projects/part2/notes/src/App.jsx
+++ projects/part2/notes/src/App.jsx
@@ -1,6 +1,6 @@
-import axios from 'axios'
 import { useState, useEffect } from 'react'
 import Note from './components/Note'
+import noteService from './services/notes'
 
 const App = () => {
   const [notes, setNotes] = useState([])
@@ -8,15 +8,12 @@ const App = () => {
   const [showAll, setShowAll] = useState(true)
 
   useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
       })
   }, [])
-  console.log('render', notes.length, 'notes')
 
   const addNote = (event) => {
     event.preventDefault()
@@ -26,27 +23,31 @@ const App = () => {
       important: Math.random() < 0.5,
     }
 
-    axios
-      .post('http://localhost:3001/notes', noteObject)
-      .then(response => {
-        setNotes(notes.concat(response.data))
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
         setNewNote('')
       })
   }
 
   const toggleImportanceOf = (id) => {
-    const url = `http://localhost:3001/notes/${id}`
     const note = notes.find(note => note.id === id)
     const changedNote = { ...note, important: !note.important }
 
-    axios
-      .put(url, changedNote)
-      .then(response => {
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
         setNotes(
-          notes.map(note => note.id === id ? response.data : note)
+          notes.map(note => note.id === id ? returnedNote : note)
         )
       })
-    console.log(`importance of ${id} needs to be toggled`)
+      .catch(() => {
+        alert(
+          `the note '${note.content}' was already deleted from server`
+        )
+        setNotes(notes.filter(n => n.id !== id))
+      })
   }
 
   const handleNoteChange = (event) => {
