266ee7ae0305caf38ffd731a5ef5d853332c8b7b
Andrew A Lee <andrew@andrewleedev.com>
2026-03-09 16:45:31 -0700

    Progress on lab part 2e

--- projects/part2/notes/src/App.jsx
+++ projects/part2/notes/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState, useEffect } from 'react'
+import Footer from './components/Footer'
 import Note from './components/Note'
 import noteService from './services/notes'
+import Notification from './components/Notification'
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('a new note...')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     noteService
@@ -43,9 +46,14 @@ const App = () => {
         )
       })
       .catch(() => {
-        alert(
-          `the note '${note.content}' was already deleted from server`
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
         )
+
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+
         setNotes(notes.filter(n => n.id !== id))
       })
   }
@@ -61,6 +69,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button type="button" onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
@@ -79,6 +88,7 @@ const App = () => {
         <input type="text" value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
       </form>
+      <Footer />
     </div>
   )
 }
--- projects/part2/notes/src/components/Note.jsx
+++ projects/part2/notes/src/components/Note.jsx
@@ -4,7 +4,7 @@ const Note = ({ note, toggleImportance }) => {
     : 'make important'
 
   return (
-    <li>
+    <li className="note">
       {note.content}&nbsp;
       <button type="button" onClick={toggleImportance}>{label}</button>
     </li>
