f675c1163b729ea6a08a1c316b04ac5675baab7b
Andrew A Lee <andrew@andrewleedev.com>
2026-07-07 19:59:03 -0700

    Completes exercise 3.4

--- exercises/part3/phonebook/index.js
+++ exercises/part3/phonebook/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const PORT = 3001
 const app = express()
-const persons = [
+let persons = [
   { 
     'id': '1',
     'name': 'Arto Hellas', 
@@ -48,6 +48,13 @@ app.get('/api/persons/:id', (request, response) => {
   }
 
   response.json(person)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
 })
 
 app.listen(PORT, () => {
