8d4a5ed547ec52507071c90eae25dce6110abd25
Andrew A Lee <andrew@andrewleedev.com>
2026-07-12 16:39:53 -0700

    Restructures the notes-backend app

---
+++ projects/part4/notes-backend/app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const { MONGODB_URI } = require('./utils/config')
+const notesRouter = require('./controllers/notes')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+
+const app = express()
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(MONGODB_URI, { family: 4 })
+  .then(() => logger.info('Connected to MongoDB'))
+  .catch(error => logger.info(`Error connecting to MongoDB: ${error.message}`))
+
+app.use(express.static('public'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/notes/', notesRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
