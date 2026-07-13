58141e0dd85e4fcfacf48a515a59184f124eb0af
Andrew A Lee <andrew@andrewleedev.com>
2026-07-12 18:42:19 -0700

    Extracts routes to controller

--- exercises/part4/blogs/index.js
+++ exercises/part4/blogs/index.js
@@ -1,27 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const { PORT, MONGODB_URI } = require('./utils/config')
-const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
 
 const app = express()
 
 mongoose.connect(MONGODB_URI, { family: 4 })
 
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
+app.use('/api/blogs', blogsRouter)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
