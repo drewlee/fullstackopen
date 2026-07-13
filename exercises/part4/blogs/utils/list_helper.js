b95ce84dada53f535bd2a75f12438d9b0bbd45f1
Andrew A Lee <andrew@andrewleedev.com>
2026-07-13 02:08:50 -0700

    Completes exercise 4.7

--- exercises/part4/blogs/utils/list_helper.js
+++ exercises/part4/blogs/utils/list_helper.js
@@ -25,8 +25,64 @@ const favoriteBlog = blogs => {
   })
 }
 
+const mostBlogs = blogs => {
+  if (!blogs.length) {
+    return null
+  }
+
+  const authorCount = new Map()
+
+  for (const blog of blogs) {
+    const count = authorCount.get(blog.author) ?? 0
+    authorCount.set(blog.author, count + 1)
+  }
+
+  const most = {
+    author: '',
+    blogs: 0,
+  };
+
+  for (const [key, value] of authorCount.entries()) {
+    if (value > most.blogs) {
+      most.author = key;
+      most.blogs = value;
+    }
+  }
+
+  return most;
+}
+
+const mostLikes = blogs => {
+  if (!blogs.length) {
+    return null
+  }
+
+  const likesCount = new Map()
+
+  for (const blog of blogs) {
+    const count = likesCount.get(blog.author) ?? 0
+    likesCount.set(blog.author, count + blog.likes)
+  }
+
+  const most = {
+    author: '',
+    likes: 0,
+  };
+
+  for (const [key, value] of likesCount.entries()) {
+    if (value > most.likes) {
+      most.author = key;
+      most.likes = value;
+    }
+  }
+
+  return most;
+}
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
+  mostLikes,
 }
