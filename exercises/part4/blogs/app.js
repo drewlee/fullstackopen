89759a71bbb27a04a646b54943a4a548f426f1b2
Andrew A Lee <andrew@andrewleedev.com>
2026-07-20 16:16:08 -0700

    Completes exercise 4.18

--- exercises/part4/blogs/app.js
+++ exercises/part4/blogs/app.js
@@ -1,15 +1,17 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const { MONGODB_URI } = require('./utils/config')
-const blogsRouter = require('./controllers/blogs')
+const loginRouter = require('./controllers/login')
 const usersRouter = require('./controllers/users')
+const blogsRouter = require('./controllers/blogs')
 
 const app = express()
 
 mongoose.connect(MONGODB_URI, { family: 4 })
 
 app.use(express.json())
-app.use('/api/blogs', blogsRouter)
+app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/blogs', blogsRouter)
 
 module.exports = app
