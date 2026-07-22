2656791b8cb860d7913733efad453d986b48b231
Andrew A Lee <andrew@andrewleedev.com>
2026-07-21 22:08:35 -0700

    Completes exercise 5.1

--- exercises/part5/bloglist-frontend/src/App.jsx
+++ exercises/part5/bloglist-frontend/src/App.jsx
@@ -1,23 +1,27 @@
 import { useState, useEffect } from 'react'
+import Blogs from './components/Blogs'
 import Blog from './components/Blog'
+import LoginForm from './components/LoginForm'
 import blogService from './services/blogs'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+      setBlogs(blogs)
+    )
   }, [])
 
+  const onLogin = (authUser) => {
+    setUser(authUser)
+  }
+
   return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
+    <>
+      {user ? <Blogs blogs={blogs} user={user} /> : <LoginForm onLogin={onLogin} />}
+    </>
   )
 }
 
---
+++ exercises/part5/bloglist-frontend/src/components/LoginForm.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+import userService from '../services/users'
+
+const LoginForm = ({ onLogin }) => {
+  const [credentials, setCredentials] = useState({ username: '', password: '' })
+
+  const handleFormSubmit = async (evt) => {
+    evt.preventDefault()
+
+    try {
+      const { username, password } = credentials
+      const user = await userService.login({ username, password })
+
+      onLogin(user)
+      setCredentials({ username: '', password: '' })
+    } catch {
+      alert('Invalid username or password')
+    }
+  }
+
+  return (
+    <div>
+      <h2>log in to application</h2>
+
+      <form onSubmit={handleFormSubmit}>
+        <div>
+          <label htmlFor="login-username">username</label>
+          <input
+            id="login-username"
+            type="text"
+            value={credentials.username}
+            onChange={(evt) => setCredentials(
+              { ...credentials, username: evt.target.value }
+            )}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="login-password">password</label>
+          <input
+            id="login-password"
+            type="password"
+            value={credentials.password}
+            onChange={(evt) => setCredentials(
+              { ...credentials, password: evt.target.value }
+            )}
+          />
+        </div>
+
+        <button type="submit">login</button>
+      </form>
+    </div>
+  )
+}
+
+export default LoginForm
