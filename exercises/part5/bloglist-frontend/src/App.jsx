59195fe35d4055e4b02e6c0aa6db1715158ececd
Andrew A Lee <andrew@andrewleedev.com>
2026-07-21 22:23:27 -0700

    Completes exercise 5.2

--- exercises/part5/bloglist-frontend/src/App.jsx
+++ exercises/part5/bloglist-frontend/src/App.jsx
@@ -4,23 +4,42 @@ import Blog from './components/Blog'
 import LoginForm from './components/LoginForm'
 import blogService from './services/blogs'
 
+const AUTH_USER_KEY = 'blogListAuthUser'
+
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [user, setUser] = useState(null)
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs(blogs)
-    )
+    const authUserValue = localStorage.getItem(AUTH_USER_KEY)
+
+    if (authUserValue) {
+      const authUser = JSON.parse(authUserValue)
+      setUser(authUser)
+    }
+
+    blogService
+      .getAll()
+      .then(blogs => setBlogs(blogs))
   }, [])
 
-  const onLogin = (authUser) => {
+  const handleOnLogin = (authUser) => {
     setUser(authUser)
+    localStorage.setItem(AUTH_USER_KEY, JSON.stringify(authUser))
+  }
+
+  const handleOnLogout = () => {
+    setUser(null)
+    localStorage.removeItem(AUTH_USER_KEY)
   }
 
   return (
     <>
-      {user ? <Blogs blogs={blogs} user={user} /> : <LoginForm onLogin={onLogin} />}
+      {
+        user
+          ? <Blogs blogs={blogs} user={user} onLogout={handleOnLogout} />
+          : <LoginForm onLogin={handleOnLogin} />
+      }
     </>
   )
 }
--- exercises/part5/bloglist-frontend/src/components/Blogs.jsx
+++ exercises/part5/bloglist-frontend/src/components/Blogs.jsx
@@ -1,11 +1,13 @@
 import Blog from './Blog'
 
-const Blogs = ({ blogs, user }) => {
+const Blogs = ({ blogs, user, onLogout }) => {
   return (
     <div>
       <h2>blogs</h2>
 
       <p>{user.name} logged in</p>
+
+      <button type="button" onClick={onLogout}>logout</button>
 
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
