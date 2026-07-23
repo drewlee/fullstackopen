9133d8991186b25de7d5196603741a214b259254
Andrew A Lee <andrew@andrewleedev.com>
2026-07-23 15:41:42 -0700

    Completes exercise 5.7

--- exercises/part5/bloglist-frontend/src/components/Blog.jsx
+++ exercises/part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,30 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+import './blog.css'
+
+const Blog = ({ blog }) => {
+  const [isVisible, setIsVisible] = useState(false)
+
+  return (
+    <article className="blog-container">
+      <div className="blog-heading">
+        <p>{blog.title} - {blog.author}</p>
+        <button type="button" onClick={() => setIsVisible(!isVisible)}>
+          {!isVisible ? 'view' : 'hide'}
+        </button>
+      </div>
+
+      <div className="blog-content" hidden={!isVisible}>
+        <ul className="blog-content-list">
+          <li className="blog-content-list-item">{blog.url}</li>
+          <li className="blog-content-list-item">
+            {blog.likes}
+            <button type="button">like</button>
+          </li>
+          <li className="blog-content-list-item">{blog.user.name}</li>
+        </ul>
+      </div>
+    </article>
+  )
+}
 
 export default Blog
