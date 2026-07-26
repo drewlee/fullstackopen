b2e5672888c9ff3723bfff79428c8c83cb65156d
Andrew A Lee <andrew@andrewleedev.com>
2026-07-26 16:25:41 -0700

    Setup Playwright for Notes app

---
+++ projects/part5/notes-tests/tests/note_app.spec.js
@@ -0,0 +1,44 @@
+const { test, expect, describe, beforeEach } = require('@playwright/test')
+
+describe('Note app', () => {
+  beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5173')
+  })
+
+  test('front page can be opened', async ({ page }) => {
+    const locator = page.getByText('Notes')
+
+    await expect(locator).toBeVisible()
+    await expect(page.getByText('Note app, Department of Computer Science, University of Helsinki 2025')).toBeVisible()
+  })
+
+  test('user can log in', async ({ page }) => {
+    await page.getByRole('button', { name: 'login' }).click()
+
+    await page.getByLabel('username').fill('jameskirk')
+    await page.getByLabel('password').fill('enterprise')
+    await page.getByRole('button', { name: 'login'}).click()
+
+    await expect(page.getByText('James T. Kirk logged in')).toBeVisible()
+  })
+
+  describe('when logged in', () => {
+    beforeEach(async ({ page }) => {
+      await page.getByRole('button', { name: 'login' }).click()
+
+      await page.getByLabel('username').fill('jameskirk')
+      await page.getByLabel('password').fill('enterprise')
+
+      await page.getByRole('button', { name: 'login' }).click()
+    })
+
+    test('a new note can be created', async ({ page }) => {
+      await page.getByRole('button', { name: 'new note' }).click()
+
+      await page.getByRole('textbox').fill('a note created by playwright')
+      await page.getByRole('button', { name: 'save' }).click()
+
+      await expect(page.getByText('a note created by playwright')).toBeVisible()
+    })
+  })
+})
