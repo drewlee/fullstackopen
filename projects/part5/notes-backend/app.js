d8fb66713b5975af1caf145c1e662eb64d85fed7
Andrew A Lee <andrew@andrewleedev.com>
2026-07-27 18:13:02 -0700

    Improves tests and adds test helpers

--- projects/part5/notes-backend/app.js
+++ projects/part5/notes-backend/app.js
@@ -4,6 +4,7 @@ const { MONGODB_URI } = require('./utils/config')
 const notesRouter = require('./controllers/notes')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
+const testingRouter = require('./controllers/testing')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 
@@ -22,6 +23,10 @@ app.use(middleware.requestLogger)
 app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/notes', notesRouter)
+
+if (process.env.NODE_ENV === 'test') {
+  app.use('/api/testing', testingRouter)
+}
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
--- projects/part5/notes-tests/tests/note_app.spec.js
+++ projects/part5/notes-tests/tests/note_app.spec.js
@@ -1,44 +1,63 @@
 const { test, expect, describe, beforeEach } = require('@playwright/test')
+const { loginWith, createNote } = require('./helper')
 
 describe('Note app', () => {
-  beforeEach(async ({ page }) => {
-    await page.goto('http://localhost:5173')
+  beforeEach(async ({ page, request }) => {
+    await request.post('/api/testing/reset')
+    await request.post('/api/users', {
+      data: {
+        name: 'James T. Kirk',
+        username: 'jameskirk',
+        password: 'enterprise',
+      },
+    })
+    await page.goto('/')
   })
 
   test('front page can be opened', async ({ page }) => {
-    const locator = page.getByText('Notes')
-
+    let locator = page.getByText('Notes')
     await expect(locator).toBeVisible()
-    await expect(page.getByText('Note app, Department of Computer Science, University of Helsinki 2025')).toBeVisible()
+
+    locator = page.getByText(
+      'Note app, Department of Computer Science, University of Helsinki 2025'
+    )
+    await expect(locator).toBeVisible()
   })
 
   test('user can log in', async ({ page }) => {
-    await page.getByRole('button', { name: 'login' }).click()
-
-    await page.getByLabel('username').fill('jameskirk')
-    await page.getByLabel('password').fill('enterprise')
-    await page.getByRole('button', { name: 'login'}).click()
-
+    await loginWith(page, 'jameskirk', 'enterprise')
     await expect(page.getByText('James T. Kirk logged in')).toBeVisible()
+  })
+
+  test('login fails with wrong password', async ({ page }) => {
+    await loginWith(page, 'jameskirk', 'wrong')
+
+    const errorDiv = page.locator('.error')
+    await expect(errorDiv).toContainText('wrong credentials')
+    await expect(errorDiv).toHaveCSS('border-style', 'solid')
+    await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)')
+    await expect(page.getByText('James T. Kirk logged in')).not.toBeVisible()
   })
 
   describe('when logged in', () => {
     beforeEach(async ({ page }) => {
-      await page.getByRole('button', { name: 'login' }).click()
-
-      await page.getByLabel('username').fill('jameskirk')
-      await page.getByLabel('password').fill('enterprise')
-
-      await page.getByRole('button', { name: 'login' }).click()
+      await loginWith(page, 'jameskirk', 'enterprise')
     })
 
     test('a new note can be created', async ({ page }) => {
-      await page.getByRole('button', { name: 'new note' }).click()
-
-      await page.getByRole('textbox').fill('a note created by playwright')
-      await page.getByRole('button', { name: 'save' }).click()
-
+      await createNote(page, 'a note created by playwright')
       await expect(page.getByText('a note created by playwright')).toBeVisible()
+    })
+
+    describe('and a note exists', () => {
+      beforeEach(async ({ page }) => {
+        await createNote(page, 'another note by playwright')
+      })
+
+      test('importance can be changed', async ({ page }) => {
+        await page.getByRole('button', { name: 'make not important' }).click()
+        await expect(page.getByText('make important')).toBeVisible()
+      })
     })
   })
 })
