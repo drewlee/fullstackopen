0d81220737538041674fa919a5328a46896e749e
Andrew A Lee <andrew@andrewleedev.com>
2026-07-27 23:45:24 -0700

    Completes exercise 5.19

--- exercises/part5/bloglist-tests/tests/bloglist_app.spec.js
+++ exercises/part5/bloglist-tests/tests/bloglist_app.spec.js
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test'
+import { loginWith, createBlog } from './helper.js'
 
 const { beforeEach, describe } = test;
 
@@ -22,9 +23,7 @@ describe('Blog app', () => {
 
   describe('Login', () => {
     test('succeeds with correct credentials', async ({ page }) => {
-      await page.getByLabel('username').fill('jameskirk')
-      await page.getByLabel('password').fill('enterprise')
-      await page.getByRole('button', { name: 'login' }).click()
+      await loginWith(page, 'jameskirk', 'enterprise')
 
       const locator = await page.getByText('Logged in as James T. Kirk')
       await locator.waitFor()
@@ -32,13 +31,74 @@ describe('Blog app', () => {
     })
 
     test('fails with wrong credentials', async ({ page }) => {
-      await page.getByLabel('username').fill('jameskirk')
-      await page.getByLabel('password').fill('invalid')
-      await page.getByRole('button', { name: 'login' }).click()
+      await loginWith(page, 'jameskirk', 'invalid')
 
       const locator = await page.getByText('Invalid username or password')
       await locator.waitFor()
       await expect(locator).toBeVisible()
+    })
+
+    describe('When logged in', () => {
+      beforeEach(async ({ page }) => {
+        await loginWith(page, 'jameskirk', 'enterprise')
+        await page.getByRole('button', { name: 'create new blog' }).click()
+      })
+
+      test('create a new blog form is shown', async ({ page }) => {
+        const locator = await page.getByRole('heading', { name: 'create new' })
+        await expect(locator).toBeVisible()
+      })
+
+      test('create a new blog form can be hidden', async ({ page }) => {
+        const locator = await page.getByRole('heading', { name: 'create new' })
+
+        await page.getByRole('button', { name: 'cancel' }).click()
+        await expect(locator).not.toBeVisible()
+      })
+
+      test('a new blog can be created', async ({ page }) => {
+        const title = 'First blog'
+        const author = 'Spock'
+        const url = 'https://spock-blog.com/first'
+  
+        await createBlog(page, title, author, url)
+
+        const blogContainer = page
+          .locator('.blog-container', { hasText: `${title} - ${author}` })
+        const blogHeading = blogContainer.locator('.blog-heading')
+
+        await expect(blogHeading).toBeVisible()
+      })
+
+      describe('When creating a new blog', () => {
+        let title;
+        let author;
+        let url;
+
+        beforeEach(async ({ page }) => {
+          title = 'First blog'
+          author = 'Spock'
+          url = 'https://spock-blog.com/first'
+
+          await createBlog(page, title, author, url)
+        })
+
+        test('blog details can be expanded', async ({ page }) => {
+          const blogContainer = page
+            .locator('.blog-container', { hasText: `${title} - ${author}` })
+          const blogHeading = blogContainer.locator('.blog-heading')
+
+          await blogHeading.getByRole('button', { name: 'view' }).click()
+
+          const blogContent = blogContainer.locator('.blog-content')
+          await expect(blogContent).toBeVisible()
+
+          const blogItems = blogContent.locator('.blog-content-list-item')
+          await expect(blogItems.nth(0)).toHaveText(url)
+          await expect(blogItems.nth(1)).toHaveText('0like')
+          await expect(blogItems.nth(2)).toHaveText('James T. Kirk')
+        })
+      })
     })
   })
 })
---
+++ exercises/part5/bloglist-tests/tests/helper.js
@@ -0,0 +1,14 @@
+export const loginWith = async (page, username, password)  => {
+  await page.getByLabel('username').fill(username)
+  await page.getByLabel('password').fill(password)
+  await page.getByRole('button', { name: 'login' }).click()
+}
+
+export const createBlog = async (page, title, author, url) => {
+  await page.getByLabel('title').fill(title)
+  await page.getByLabel('author').fill(author)
+  await page.getByLabel('url').fill(url)
+
+  await page.getByRole('button', { name: 'create' }).click()
+  await page.getByText(`${title} - ${author}`).waitFor()
+}
