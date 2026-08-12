98c2ead3c999f35e2292043f2fb52f876ab89281
Andrew A Lee <andrew@andrewleedev.com>
2026-08-11 20:22:25 -0700

    Completes exercise 7.1

--- exercises/part7/routed-anecdotes/src/components/CreateNew.jsx
+++ exercises/part7/routed-anecdotes/src/components/CreateNew.jsx
@@ -1,33 +1,40 @@
-import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useField } from '../hooks/index'
 
 const CreateNew = ({ addAnecdote }) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
   const navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    addAnecdote({ content, author, info, votes: 0 })
+
+    addAnecdote({
+      content: content.value,
+      author: author.value,
+      info: info.value,
+      votes: 0,
+    })
     navigate('/')
   }
 
   return (
     <div>
       <h2>create a new anecdote</h2>
+
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input name="content" {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input name="author" {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e) => setInfo(e.target.value)} />
+          <input name="info" {...info} />
         </div>
         <button>create</button>
       </form>
